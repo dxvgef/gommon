5de1f2e05b68bdd356daff7393f1172ddc72db84
dxvgef <dxvgef@outlook.com>
2019-08-03 17:58:11 +0800

    新增CompareStrings函数

--- slice/compare.go
+++ slice/compare.go
@@ -1,1 +1,17 @@
 package slice
+
+// Compare 比较两个strings slice，返回add新增元素和remove移除的元素
+func CompareStrings(old, new []string) (add []string, remove []string) {
+	for k := range new {
+		if InStr(old, new[k]) == false {
+			add = append(add, new[k])
+		}
+	}
+	for k := range old {
+		if InStr(new, old[k]) == false {
+			remove = append(remove, old[k])
+		}
+	}
+
+	return
+}
--- slice/compare_test.go
+++ slice/compare_test.go
@@ -1,1 +1,11 @@
 package slice
+
+import "testing"
+
+func TestCompareStrings(t *testing.T) {
+	old := []string{"111", "aaa", "bbb", "ccc"}
+	new := []string{"aaa", "ccc", "ddd", "eee"}
+	add, remove := CompareStrings(old, new)
+	t.Log(add)
+	t.Log(remove)
+}
